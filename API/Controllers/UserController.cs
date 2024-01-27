4de7bfc85bfd4d7fcae232603d76958a94fda978
nishantketu1125 <shikhakumari212002@gmail.com>
2024-01-27 13:11:04 +0530

    New Project

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using API.Data;
+using API.Entity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+
+[Route("api/[controller]")]
+
+public class UserController : ControllerBase
+{
+    private readonly AppUserData _appUser;
+    public UserController(AppUserData appUser)  
+    {
+        _appUser = appUser;
+    }  
+     
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUser()
+    {
+        var users = await _appUser.Users.ToListAsync();
+
+        return users;
+
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AppUser>>GetUserById(int id)
+    {
+        return await _appUser.Users.FindAsync(id);
+    }    
+
+}
---
+++ API/Data/AppUserData.cs
@@ -0,0 +1,15 @@
+﻿using API.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class AppUserData : DbContext
+    {
+        public AppUserData(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
+
