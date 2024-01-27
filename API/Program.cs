0881dad7e0d35369bef64a6cb2a7fb705b5ddb70
nishantketu1125 <shikhakumari212002@gmail.com>
2024-01-27 22:39:07 +0530

    CHANGE

--- API/Program.cs
+++ API/Program.cs
@@ -5,16 +5,19 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
 builder.Services.AddDbContext<AppUserData>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
-app.UseHttpsRedirection();
+//Configure HTTP request
 
-app.UseAuthorization();
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.MapControllers();
 
